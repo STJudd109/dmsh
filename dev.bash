95436f64bec2421b6756e3207532dff860ce09c8
Spencer Judd <spencerjfam@hotmail.com>
2016-08-20 13:32:22 -0600

    init commit

---
+++ dev.bash
@@ -0,0 +1,25 @@
+ #!/bin/bash          
+
+$ vi ginfo
+#
+#
+# Script to print user information who currently login , current date & time
+#
+clear
+echo "Calendar"
+cal
+echo "###############################"
+echo "#"
+echo "#"
+echo "Welcome to Devmountain!"
+echo "Hello $USER"
+echo "today is ";date
+echo "#"
+echo "#"
+echo "lets do this!!"
+echo "################################"
+myDir='Desktop/Devmountain/'
+Open 'https://github.com/DevMountain/DMWebAH-Syllabus'
+Open ~/$myDir/
+open -a Terminal ~/$myDir/
+exit 0
